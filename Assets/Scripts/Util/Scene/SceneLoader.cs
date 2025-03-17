bf22adcb05abd171af59d18260f465ba450cbb7e
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-03-18 00:38:10 +0900

    load Scene 테스트

--- Assets/Scripts/Util/Scene/SceneLoader.cs
+++ Assets/Scripts/Util/Scene/SceneLoader.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Events;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public UnityEvent onStartLoadSceneEvent;
 
+
+    public string loadSceneAddress;
     [SerializeField]
     private string NextSceneAddress;
     private bool isLoadScene = false;
 
-    private AsyncOperationHandle sceneOperation;
+    private AsyncOperationHandle<SceneInstance> sceneOperation;
 
 
     private void OnDestroy()
@@ -23,36 +27,65 @@ public class SceneLoader : MonoBehaviour
         // Addressables.Release(sceneOperation);
     }
 
-    private IEnumerator CoInitAddressables()
+    public void SwitchScene(string nextScene, bool useFade = true)
+    {
+        NextSceneAddress = nextScene;
+        if (useFade)
+        {
+            LoadScene(loadSceneAddress);
+            //FadeController.Instance.FadeIn(() =>
+            //{
+            //    LoadScene(loadSceneAddress);
+            //});
+        }
+        else
+        {
+            LoadScene(loadSceneAddress);
+        }
+    }
+
+    private void LoadScene(string nextScene)
+    {
+        StartCoroutine(CoInitAddressables(nextScene));
+    }
+
+    private void LoadNextScene(string nextScene)
+    {
+        StartCoroutine(CoInitAddressables(nextScene, false));
+    }
+
+    private IEnumerator CoInitAddressables(string nextScene, bool loadScene = true)
     {
         var initializeAsync = Addressables.InitializeAsync();
         yield return initializeAsync;
 
-        var downloadOperation = Addressables.DownloadDependenciesAsync("default");
-
-        while (!downloadOperation.IsDone)
-        {
-            Debug.Log(downloadOperation.PercentComplete * 100f);
-            yield return null;
-        }
-
-        if (downloadOperation.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Failed)
-        {
-            Debug.LogError("다운로드 실패");
-            yield break;
-        }
-
-        Addressables.Release(downloadOperation);
-        sceneOperation = Addressables.LoadSceneAsync(NextSceneAddress, LoadSceneMode.Single);
+        sceneOperation = Addressables.LoadSceneAsync(nextScene, LoadSceneMode.Single, false);
 
         while (!sceneOperation.IsDone)
         {
             Debug.Log(sceneOperation.PercentComplete * 100f);
 
-            yield return null;
-
+            yield return new WaitForSeconds(0.5f);
         }
 
+        
+        sceneOperation.Result.ActivateAsync();
+
+
+        if (loadScene)
+        {
+            var objectList = sceneOperation.Result.Scene.GetRootGameObjects();
+
+            foreach (var @object in objectList)
+            {
+                var sceneLoader = @object.GetComponent<SceneLoader>();
+                if (sceneLoader != null)
+                {
+                    sceneLoader.LoadNextScene(NextSceneAddress);
+                    break;
+                }
+            }
+        }
         Addressables.Release(sceneOperation);
     }
 
@@ -61,8 +94,9 @@ public class SceneLoader : MonoBehaviour
         if (!isLoadScene)
         {
             // Addressables.LoadSceneAsync(NextSceneAddress);
+            SwitchScene(NextSceneAddress);
 
-            StartCoroutine(CoInitAddressables());
+            // StartCoroutine(CoInitAddressables(NextSceneAddress));
             onStartLoadSceneEvent?.Invoke();
             isLoadScene = true;
         }
