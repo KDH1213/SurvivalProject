11a2e9f3bd15e42b4eb33ec78dee26e975f71432
ParkHaYoung <rudal4277@naver.com>
2025-03-20 18:52:41 +0900

    [chore] 테스트 씬 수정
    1. 테스트 버튼(공격, 앉기) 추가
    2. 조이스틱 스크립트 추가

---
+++ Assets/Scripts/HY/FloatingJoystickInput.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystickInput : MonoBehaviour
+{
+    [SerializeField] private RectTransform joystickBackground; // 조이스틱 배경
+    [SerializeField] private RectTransform joystickHandle;     // 조이스틱 핸들
+    [SerializeField] private RectTransform joystickArea;       // 조이스틱이 활성화될 우측 하단 영역
+    [SerializeField] private float moveRange = 100f;           // 조이스틱 이동 반경
+    [SerializeField] private OnScreenStick onScreenStick;      // 입력 시스템용 조이스틱
+
+    private Vector2 initialPosition;
+    private bool isUsingMouse = false;
+    private bool isActive = false; // 조이스틱 활성화 여부
+
+    private void Start()
+    {
+        initialPosition = joystickBackground.anchoredPosition;
+    }
+
+    private void Update()
+    {
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            HandleTouchInput();
+        }
+        else if (Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            HandleMouseInput();
+        }
+        else
+        {
+            ResetJoystick();
+        }
+    }
+
+    private void HandleTouchInput()
+    {
+        var touch = Touchscreen.current.primaryTouch;
+        Vector2 touchPosition = touch.position.ReadValue();
+
+        if (!isActive)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(joystickArea, touchPosition))
+            {
+                ActivateJoystick(touchPosition);
+            }
+        }
+        else
+        {
+            MoveJoystick(touchPosition);
+        }
+    }
+
+    private void HandleMouseInput()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        if (!isUsingMouse)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(joystickArea, mousePosition))
+            {
+                ActivateJoystick(mousePosition);
+                isUsingMouse = true;
+            }
+        }
+        else
+        {
+            MoveJoystick(mousePosition);
+        }
+    }
+
+    private void ActivateJoystick(Vector2 position)
+    {
+        joystickBackground.position = position;
+        joystickBackground.gameObject.SetActive(true);
+        isActive = true;
+    }
+
+    private void MoveJoystick(Vector2 position)
+    {
+        Vector2 direction = position - (Vector2)joystickBackground.position;
+        Vector2 clampedPosition = Vector2.ClampMagnitude(direction, moveRange);
+        joystickHandle.anchoredPosition = clampedPosition;
+
+        onScreenStick.OnDrag(new PointerEventData(EventSystem.current)
+        {
+            position = position
+        });
+    }
+
+    private void ResetJoystick()
+    {
+        isActive = false;
+        isUsingMouse = false;
+        joystickBackground.anchoredPosition = initialPosition;
+        joystickHandle.anchoredPosition = Vector2.zero;
+
+        onScreenStick.OnPointerUp(new PointerEventData(EventSystem.current));
+    }
+}
--- Assets/Scripts/HY/TestController.cs
+++ Assets/Scripts/HY/TestController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
 
 public class TestController : MonoBehaviour
 {
+    public static readonly int hashSpeed = Animator.StringToHash("Speed");
+    public static readonly int hashWalk = Animator.StringToHash("walk");
+
+
     [SerializeField]
     private float moveSpeed;
     [SerializeField]
@@ -14,51 +19,64 @@ public class TestController : MonoBehaviour
     [SerializeField]
     private GameObject target;
 
-    private Vector2 m_Move;
-    private Vector2 m_Rotate;
+    private Vector2 move;
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        m_Move = context.ReadValue<Vector2>();
+        move = context.ReadValue<Vector2>();
     }
 
-    public void OnRotate(InputAction.CallbackContext context)
-    {
-        m_Rotate = context.ReadValue<Vector2>();
-    }
 
     public void OnColorChange(InputAction.CallbackContext context)
     {
-        if(context.phase == InputActionPhase.Performed)
+        if (context.phase == InputActionPhase.Performed)
         {
             Color color = Random.ColorHSV();
             ChangeColor(color);
         }
     }
 
+    public void OnSit(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            target.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+
     private void Update()
     {
-        Move(m_Move);
-        Rotate(m_Rotate);
+        MoveAndRotate(move);
+        //animator.SetFloat(hashSpeed, agent.velocity.magnitude);
     }
 
-    private void Move(Vector2 direction)
+
+    private void MoveAndRotate(Vector2 direction)
     {
-        if (direction.sqrMagnitude < 0.01)
-            return;
-
-        var scaleMoveSpeed = moveSpeed * Time.deltaTime;
-
-        var move = Quaternion.Euler(0, target.transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
-        target.transform.position += move * scaleMoveSpeed;
-    }
-
-    private void Rotate(Vector2 direction)
-    {
-        if (direction.sqrMagnitude > 0.01f) // 입력이 있는 경우에만 실행
+        if (direction.sqrMagnitude < 0.01f)
         {
-            float rotationAmount = rotateSpeed * direction.x * Time.deltaTime;
-            target.transform.Rotate(Vector3.up * rotationAmount);
+            animator.SetBool(hashWalk, false);
+            return;
+        }
+        else
+        {
+            animator.SetBool(hashWalk, true);
+
+            // 이동 벡터 계산 (월드 좌표 기준)
+            Vector3 moveDir = new Vector3(direction.x, 0, direction.y).normalized;
+            target.transform.position += moveDir * (moveSpeed * Time.deltaTime);
+
+            // 이동하는 방향으로 캐릭터 회전
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            target.transform.rotation = Quaternion.Slerp(target.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
         }
     }
 
