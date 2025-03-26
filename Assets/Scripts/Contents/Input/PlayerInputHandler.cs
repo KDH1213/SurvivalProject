98ac7385b34b11326927e06ec3d832eaf63e3dcf
ParkHaYoung <rudal4277@naver.com>
2025-03-26 15:05:20 +0900

    [chore] 인풋 시스템 통합 및 폴더링

---
+++ Assets/Scripts/Contents/Input/PlayerInputHandler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector2 inputDirection;
+
+    private PlayerFSM playerFSM;
+
+    public UnityEvent onAttackEvent;
+    public UnityEvent onInteractEvent;
+    public UnityEvent<Vector2> onMoveAndRotateEvent;
+
+    private void Start()
+    {
+        playerFSM = GetComponent<PlayerFSM>();
+    }
+
+    // TODO :: TestPlayer -> PlayerInput -> Events -> player -> moveAndRotate에 연결
+    public void OnMoveAndRotate(InputAction.CallbackContext context)
+    {
+        inputDirection = context.ReadValue<Vector2>();
+        onMoveAndRotateEvent?.Invoke(inputDirection);
+    }
+
+    // TODO :: TestPlayer -> PlayerInput -> Events -> player -> mainToolAndAttack에 연결
+    public void OnMainToolUsedAndAttck(InputAction.CallbackContext context)
+    {
+        if (playerFSM.CanAttack && context.phase == InputActionPhase.Performed)
+        {
+            onAttackEvent?.Invoke();
+        }
+    }
+
+    // TODO :: TestPlayer -> PlayerInput -> Events -> player -> interact에 연결
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (playerFSM.IsPlayerInRange && context.phase == InputActionPhase.Performed)
+        {
+            onInteractEvent?.Invoke();
+        }
+    }
+
+    // TODO :: TestPlayer -> PlayerInput -> Events -> player -> subTool에 연결
+    public void OnSubToolUsed(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+
+        }
+    }
+
+    // TODO :: TestPlayer -> PlayerInput -> Events -> player -> Building에 연결
+    public void OnBuild(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+
+        }
+    }
+}
