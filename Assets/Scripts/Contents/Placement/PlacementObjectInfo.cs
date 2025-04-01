8831f587a4a9278b8063b51dfde5c9726bf8e21b
신규현 <skh001218@naver.com>
2025-04-01 19:13:39 +0900

    [chore] 오브젝트 구조 변경

--- Assets/Scripts/Contents/Placement/PlacementObjectInfo.cs
+++ Assets/Scripts/Contents/Placement/PlacementObjectInfo.cs
@@ -8,20 +8,29 @@ using UnityEngine;
 public class PlacementObjectInfo
 {
     [field: SerializeField]
-    public string Name { get; private set; }
-    [field: SerializeField]
     public int ID { get; private set; }
-    [field: SerializeField]
-    public int DefaultHp { get; private set; }
     [field: SerializeField]
     public int MaxBuildCount { get; private set; }
     [field: SerializeField]
     public Vector2Int Size { get; private set; } = Vector2Int.one;
     [field: SerializeField]
+    public StructureKind Kind { get; private set; }
+    [field: SerializeField]
+    public List<PlacementLevelInfo> LevelList  { get; private set; }
+
+}
+
+[Serializable]
+public class PlacementLevelInfo
+{
+    [field: SerializeField]
+    public string Name { get; private set; }
+    [field: SerializeField]
+    public int DefaultHp { get; private set; }
+    [field: SerializeField]
     public GameObject Prefeb { get; private set; }
     [field: SerializeField]
-    public StructureKind kind { get; private set; }
-    [field: SerializeField]
     public SerializedDictionary<string, int> NeedItems { get; private set; }
-
+    [field: SerializeField]
+    public Sprite Icon { get; private set; }
 }
