159864365657c46df235ffa5157dafb159956fa3
김동현 <ehdgus0710@naver.com>
2025-04-02 19:18:21 +0900

    [chore] 플레이어 애니메이션 이벤트 리스너 추가

---
+++ Assets/Scripts/Contents/Util/PlayerAnimationEventListener.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerAnimationEventListener : MonoBehaviour
+{
+    public UnityEvent startAttackEvent;
+    public UnityEvent endAttackAnimationEvent;
+    public UnityEvent createAttackVFXEvnet;
+
+    public void OnStartAttack()
+    {
+        startAttackEvent?.Invoke();
+    }
+
+    public void OnEndAttackAnimation()
+    {
+        endAttackAnimationEvent?.Invoke();
+    }
+
+    public void OnCreateVFX()
+    {
+        createAttackVFXEvnet?.Invoke();
+    }
+}
