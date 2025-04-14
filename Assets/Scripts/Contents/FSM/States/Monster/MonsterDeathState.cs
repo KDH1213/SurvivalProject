af3ff6142ddbc1644b90e75f9c5c71f08a410538
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-04-14 23:02:32 +0900

    [chore] 몬스터 사망 시 콜라이더 비활성화 추가

--- Assets/Scripts/Contents/FSM/States/Monster/MonsterDeathState.cs
+++ Assets/Scripts/Contents/FSM/States/Monster/MonsterDeathState.cs
@@ -1,14 +1,19 @@
+using UnityEngine;
+
 public class MonsterDeathState : MonsterBaseState
 {
     //[SerializeField]
     //private Inventory inventory;
 
     // public List<ItemData> items;
+    private Collider ownerCollider;
+
 
     protected override void Awake()
     {
         base.Awake();
         stateType = MonsterStateType.Death;
+        ownerCollider = GetComponent<Collider>();
     }
 
     public override void Enter()
@@ -16,7 +21,9 @@ public class MonsterDeathState : MonsterBaseState
         MonsterFSM.OnEndInteractEvent?.Invoke(gameObject);
 
         MonsterFSM.Animator.SetTrigger(MonsterAnimationHashCode.hashDeath);
-        MonsterFSM.Animator.SetBool(MonsterAnimationHashCode.hashIsDeath, true);
+        // MonsterFSM.Animator.SetBool(MonsterAnimationHashCode.hashIsDeath, true);
+
+        ownerCollider.enabled = false;
     }
 
     public override void ExecuteUpdate()
@@ -31,5 +38,6 @@ public class MonsterDeathState : MonsterBaseState
     public void OnEndAnimationEvent()
     {
         gameObject.SetActive(false);
+        ownerCollider.enabled = true;
     }
 }
