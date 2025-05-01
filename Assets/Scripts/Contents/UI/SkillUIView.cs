51d1345e3b20286389806513f599f2a1545dc81e
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-05-01 22:46:13 +0900

    [chore] using 제거 및 철 자원 수정

--- Assets/Scripts/Contents/UI/SkillUIView.cs
+++ Assets/Scripts/Contents/UI/SkillUIView.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SkillUIView : MonoBehaviour, ISaveLoadData
