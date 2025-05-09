7198e220b986617ca2be81e2a846a0b4284b7750
김동현 <ehdgus0710@naver.com>
2025-05-09 11:36:21 +0900

    [chore] 스프라이트 생성 에디터 수정 및 아이콘 추가

--- Assets/Scripts/Editor/CreateSpriteEditor.cs
+++ Assets/Scripts/Editor/CreateSpriteEditor.cs
@@ -3,12 +3,46 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class CreateSpriteEditor : MonoBehaviour
+public class CreateSpriteEditor : EditorWindow
 {
     [SerializeField]
     private GameObject[] createObjects;
 
     private readonly string path = "Assets/Resources/Sprites/Create";
+    private SerializedObject serializedObject;
+
+    private Vector2 scrollPos = Vector2.zero;
+
+    private void OnEnable()
+    {
+        ScriptableObject target = this;
+        serializedObject = new SerializedObject(target);
+    }
+
+    [MenuItem("Tools/CreateSpriteEditor")]
+    public static void ShowWindow()
+    {
+        CreateSpriteEditor window = (CreateSpriteEditor)GetWindow(typeof(CreateSpriteEditor), false, "CreateSpriteEditor", true);
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        GUILayout.Label("CreateSprite:");
+        serializedObject.Update();
+        SerializedProperty titleProperty = serializedObject.FindProperty("createObjects");
+        EditorGUILayout.PropertyField(titleProperty, true);
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUILayout.Button("Button"))
+        {
+            GetPreview();
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+
 
     [ContextMenu("CreateSprite")]
     public void GetPreview()
